d6afc47f46cff3570883b9b8f28b90baf0f62601
eleotard <eleotard@student.42.fr>
2024-12-29 19:17:46 +0100

    euuuh

--- predict.py
+++ predict.py
@@ -1,5 +1,7 @@
 import json
 
+
+
 def check_user_input(input):
     try:
         mileage = float(input)
@@ -11,23 +13,24 @@ def check_user_input(input):
         print("Unexpected error occured")
         return 0
     
-mileage = input("Enter a mileage: ")
-while not check_user_input(mileage):
-    mileage = input("Enter a mileage: ")
 
-teta0 = 0.0
-teta1 = 0.0
+theta0 = 0.0
+theta1 = 0.0
 
 try:
     with open("train_result.json", 'r') as train_res:
         data = json.load(train_res)
-    teta0 = data["teta0"]
-    teta1 = data["teta1"]
+    theta0 = data["theta0"]
+    theta1 = data["theta1"]
 except:
-    print("0")
-
-estimatePrice(mileage) = teta0 + (teta1) * mileage
-print("For a car with {mileage} mileages, the estimate price is : {estimatePrice}")
+    pass
+    
+mileage = input("Enter a mileage: ")
+while not check_user_input(mileage):
+    mileage = input("Enter a mileage: ")
+mileage = float(mileage)
+estimatePrice = theta0 + (theta1) * mileage
+print(f"For a car with {mileage} mileages, the estimate price is : {estimatePrice}")
 
 
 
--- train.py
+++ train.py
@@ -1,14 +1,64 @@
 import pandas as pd 
 import json
+import sys
+import random
 
-df = pd.read_csv('data.csv')
+expected_keys = {'km', 'price'}
 
-# print(df["km"])
-# print(type(df["km"]))
+def checkDataFrame(df):
+    if (set(df.columns) != expected_keys):
+        print("ERROR: Wrong keys in the dataset.")
+        return 0
+    for column in df.columns:
+        #print(df[column])
+        try:
+            pd.to_numeric(df[column])
+        except ValueError:
+            print("ERROR: There are some columns in the dataset that contains none numeric values.")
+            return 0
+
+    # if (df.dtypes['km'] != "int64" and df.dtypes['km'] != "float64") or (df.dtypes['price'] != 'int64' and df.dtypes['price'] != 'float64'):
+    #     return 0
+    return 1
+
+def estimatePrice(tmpθ0, tmpθ1, mileage):
+    return tmpθ1 * mileage + tmpθ0   
+
+try:
+    df = pd.read_csv('data.csv')
+except:
+    print("ERROR: no dataset found.")
+    sys.exit(1)
+
+if not checkDataFrame(df):
+    sys.exit(1)
+
+df = df.dropna()
+tmpθ0 = random.random()
+tmpθ1 = random.random()
+lear_rate = 0.0000001
+m = len(df) #24
+#print(m)
+
+for i in range(10):
+    added_distance_0 = 0.0
+    added_distance_1 = 0.0
+    #calculer la somme avec valeurs retournees par iterrows
+    #iterrows retourne un tuple avec 2 elements
+    for index, row in df.iterrows(): #index #Series: ligne avec valeurs associees aux noms de colonne
+        predicted_price = estimatePrice(tmpθ0, tmpθ1, row['km'])
+        error = predicted_price - row['price']
+        added_distance_0 += abs(error)
+        added_distance_1 += abs(error) * row["km"]
+    tmpθ0 = (lear_rate * added_distance_0 / m)
+    tmpθ1 = (lear_rate * added_distance_1 / m)
+
+print(tmpθ0)
+print(tmpθ1)
 
 data = {
-    "teta0": 0.1,
-    "teta1": 0.6,
+    "theta0": tmpθ0,
+    "theta1": tmpθ1,
 }
 
 with open('train_result.json', 'w') as file:
